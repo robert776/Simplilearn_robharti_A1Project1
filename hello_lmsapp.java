660adff50db366aa2de8d6591cc4f13961572ad3
robert776 <rbbharti776@gmal.com>
2023-05-10 16:46:50 +0530

    Update hello_lmsapp.java
    Added print statement

--- hello_lmsapp.java
+++ hello_lmsapp.java
@@ -2,6 +2,7 @@ class hello_lmsapp {
     public static void main(String[] args) {
 	  System.out.println("Hi Simplilearn lms - PG devops program");
           System.out.println("This is a java app created using Dockerfile");
+	  System.out.println("Assessment project ");  
 	  System.out.println("Author :- Robert Bharti,rbbharti776@gmail.com");
     }
   }
